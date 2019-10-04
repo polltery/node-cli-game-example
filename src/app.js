91c83a885a6793b8ea8e55ce389b1c80c86272bb
ufko <ufukozcelik8@gmail.com>
2019-10-04 20:06:08 +0300

    Move create2dArray to utils

--- src/app.js
+++ src/app.js
@@ -232,16 +232,3 @@ function encounter(object){
             break;
     }
 }
-
-function create2dArray(numrows, numcols, initial){
-
-    var arr = [];
-    for (var i = 0; i < numrows; ++i){
-       var columns = [];
-       for (var j = 0; j < numcols; ++j){
-          columns[j] = initial;
-       }
-       arr[i] = columns;
-    }
-    return arr;
- }
