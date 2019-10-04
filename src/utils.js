4457f6aa15dc498cc4cf8339cc61e69c3f4338cc
ufko <ufukozcelik8@gmail.com>
2019-10-04 22:09:15 +0300

    Move card type assignments to utils

--- src/utils.js
+++ src/utils.js
@@ -9,8 +9,30 @@ function create2dArray(numrows, numcols, initial){
        arr[i] = columns;
     }
     return arr;
- }
+}
 
- module.exports = {
-    create2dArray
- };
+function assignTypesToCards() {
+    const monsters = require('./lib/monsters.js');
+    const bosses = require('./lib/bosses.js');
+    const specials = require('./lib/specials.js');
+    const friends = require('./lib/friends.js');
+
+    // Add type because they are going to be merged in the map
+    for(var i = 0; i < monsters.length; i++){
+        monsters[i].type = "monster";
+    }
+    for(var i = 0; i < bosses.length; i++){
+        bosses[i].type = "boss";
+    }
+    for(var i = 0; i < specials.length; i++){
+        specials[i].type = "special";
+    }
+    for(var i = 0; i < friends.length; i++){
+        friends[i].type = "friend";
+    }
+}
+
+module.exports = {
+    create2dArray,
+    assignTypesToCards
+};
